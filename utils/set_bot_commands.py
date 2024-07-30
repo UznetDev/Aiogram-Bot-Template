c5fc4cedd2521d3056ab18a0b893a61a8e25ba96
UznetDev <uznattv@gmail.com>
2024-07-30 13:16:17 +0500

    update set_bot_commands.py

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -5,7 +5,5 @@ async def set_default_commands():
     await bot.set_my_commands(
         [
             types.BotCommand(command='start', description='♻Start bot..'),
-            types.BotCommand(command='help', description='📜Help and Information..'),
-            types.BotCommand(command='my', description='📻Your playlist..')
         ]
     )
