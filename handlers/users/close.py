b91aef456ca6ecc722b7b147dcae86b5153ed7bf
UznetDev <uznattv@gmail.com>
2024-07-30 16:28:58 +0500

    Add comment

--- handlers/users/close.py
+++ handlers/users/close.py
@@ -7,9 +7,24 @@ from aiogram import types, F
 
 @dp.callback_query(MainCallback.filter(F.action == "close"))
 async def close(call: types.CallbackQuery, state: FSMContext):
+    """
+    Handles the close callback action.
+    Clears the FSM context and deletes the message that triggered the callback query.
+
+    Args:
+        call (types.CallbackQuery): The callback query.
+        state (FSMContext): The FSM context.
+
+    Returns:
+        None
+    """
     try:
+        # Clear the FSM context
         await state.clear()
+
+        # Delete the message that triggered the callback query
         await bot.delete_message(chat_id=call.from_user.id,
                                  message_id=call.message.message_id)
     except Exception as err:
-        logging.error(err)
+        logging.error(f"Error in close handler: {err}")
+
