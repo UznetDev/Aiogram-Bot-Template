3debbc8915b49dedd3383f8e8a6f4b799fada6c0
UznetDev <uznattv@gmail.com>
2024-07-30 17:28:25 +0500

    Add comment

--- data/config.py
+++ data/config.py
@@ -2,18 +2,22 @@ import datetime
 from environs import Env
 from utils.db_api.bot_db import BotDb
 
+# Initialize the Env object to read environment variables
 env = Env()
 env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")
-ADMIN = env.int("ADMIN")
-HOST = env.str('HOST')
-MYSQL_USER = env.str('MYSQL_USER')
-MYSQL_PASSWORD = env.str('MYSQL_PASSWORD')
-MYSQL_DATABASE = env.str('MYSQL_DATABASE')
+# Read environment variables for bot configuration
+BOT_TOKEN = env.str("BOT_TOKEN")  # The token for the Telegram bot
+ADMIN = env.int("ADMIN")  # The ID of the super admin
+HOST = env.str('HOST')  # The host for the database
+MYSQL_USER = env.str('MYSQL_USER')  # The MySQL database user
+MYSQL_PASSWORD = env.str('MYSQL_PASSWORD')  # The MySQL database password
+MYSQL_DATABASE = env.str('MYSQL_DATABASE')  # The MySQL database name
 
+# Get the current date and time
 datas = datetime.datetime.now()
-yil_oy_kun = (datetime.datetime.date(datetime.datetime.now()))
-soat_minut_sekund = f"{datas.hour}:{datas.minute}:{datas.second}"
+yil_oy_kun = datetime.datetime.date(datetime.datetime.now())  # The current date (year-month-day)
+soat_minut_sekund = f"{datas.hour}:{datas.minute}:{datas.second}"  # The current time (hour:minute:second)
 
-log_file_name = 'db/logging.log'
+# Define the log file name and path
+log_file_name = 'db/logging.log'  # The log file path for logging database activities
