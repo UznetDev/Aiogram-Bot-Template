5e5c5a223e1de67f35a135bcf2a6c161b38d3dfb
UznetDev <uznattv@gmail.com>
2024-07-30 13:33:10 +0500

    update check_join.py

--- handlers/users/check_join.py
+++ handlers/users/check_join.py
@@ -13,7 +13,7 @@ async def check_join(call: types.CallbackQuery):
         lang = call.from_user.language_code
         ruyxat = db.select_channels()
         btn = InlineKeyboardBuilder()
-        text = translator(text="<b>🛑 You are not join the channel!:\n\n</b>",
+        text = translator(text="🛑 You are not join the channel!:\n\n",
                           dest=lang)
         text1 = translator(text="🛑 You are not join the channel!:\n\n",
                            dest=lang)
@@ -31,7 +31,7 @@ async def check_join(call: types.CallbackQuery):
             else:
                 force = True
                 son += 1
-                text += f"{son}<b><i></i>. ⭕  {kanals.full_name}</b> <i>@{kanals.username} ❓</i>\n"
+                text += f"\n{son}<b><i></i>. ⭕  {kanals.full_name}</b> <i>@{kanals.username} ❓</i>\n"
                 btn.button(text='➕ ' + kanals.title,
                            url=f"{await kanals.export_invite_link()}")
         btn.button(text=translator(text='♻ Check!', dest=lang),
@@ -43,29 +43,12 @@ async def check_join(call: types.CallbackQuery):
                               reply_markup=btn.as_markup())
             await bot.edit_message_text(chat_id=cid,
                                         message_id=call.message.message_id,
-                                        text=text,
+                                        text=f"<b>{text}</b>",
                                         reply_markup=btn.as_markup())
         else:
-            # bot_info = await bot.get_me()
             lang = call.from_user.language_code
-            text = translator(text='<b>👋Hello dear ',
-                              dest=lang) + f' {call.from_user.full_name}\n\n</b>'
-
-            tx = '<b>😎<i> I can download any music ' \
-                 'All music with real data.</i></b>\n' \
-                 '     🔎<i> I can find music from Instagram, TikTok, Twitter, Threads, Video, Audio.\n' \
-                 '      ‼For this, just send me the video address or audio, video file or just send me the song name.\n' \
-                 '     🔍 I can also search for music on YouTube.\n' \
-                 '     🎙All songs are downloaded in 128 kbps (MP3) and with original artwork.\n' \
-                 '     📜if the lyrics are available i can send them!\n\n' \
-                 '     ⚠<u>If the audio was uploaded by me first, I will send it instantly, otherwise it may take up to 10 seconds to upload.</u>\n\n' \
-                 '     📥 I will post the downloaded audios on my channel. ' \
-                 '<b><a href="https://t.me/YouTube_Downs">🔗YouTube</a>\t  ' \
-                 '  <a href="https://t.me/Spotify_Music_down">🔗Spotify</a></b>\n\n' \
-                 '<b>👩‍💻Help :) Creators channel: <a href="https://t.me/UZ_NET_Coder">UZ_NET_Coder</a></b></i>'
-            text += translator(text=tx,
-                               dest=lang)
-
+            text = translator(text='Same text',
+                              dest=lang)
             await bot.edit_message_text(chat_id=cid,
                                         message_id=call.message.message_id,
                                         text=text)
