bd2ea0bc3d560bf50e8c9f03ecc301da0fa349c2
UznetDev <uznattv@gmail.com>
2024-07-30 17:29:20 +0500

    Add comment

--- loader.py
+++ loader.py
@@ -1,15 +1,20 @@
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
-from data.config import *
+from data.config import *  # Import all configurations from the config module
 from utils.db_api.mysql_db import Database
 
+# Initialize the Telegram bot with the given token and parse mode set to HTML
+bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
 
-bot = Bot(token=BOT_TOKEN,
-          parse_mode=ParseMode.HTML)
+# Initialize memory storage for the dispatcher
 storage = MemoryStorage()
+
+# Initialize the dispatcher with the memory storage
 dp = Dispatcher(storage=storage)
 
+# Initialize the MySQL database connection
 db = Database(host=HOST, user=MYSQL_USER, password=MYSQL_PASSWORD, database=MYSQL_DATABASE)
 
+# Initialize the BotDb with the specified JSON file
 DB = BotDb(file='data/db.json')
