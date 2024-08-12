45b27d54cd2d0f4543aec1986bec97d8be2d82d0
UznetDev <uznattv@gmail.com>
2024-08-12 16:32:01 +0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 
-
 extensions = [
     Extension(name="cython_code.my_translator", sources=["cython_code/my_translator.pyx"]),
     Extension(name="cython_code.user_check", sources=["cython_code/user_check.pyx"]),
     Extension(name="cython_code.throttling_middleware", sources=["cython_code/throttling_middleware.pyx"]),
     Extension(name="cython_code.file_db", sources=["cython_code/file_db.pyx"]),
-
+    Extension(name="cython_code.send_ads", sources=["cython_code/send_ads.pyx"]),  # Corrected
 ]
 
 setup(
@@ -16,5 +15,7 @@ setup(
     packages=["cython_code"],
     zip_safe=False,
 )
+
+
 
 # python setup.py build_ext --inplace
