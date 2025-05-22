53bbf437e60ed8ec2d75ce737ee5ec8ba1d10043
UZNetDev <uznetdev@gmail.com>
2025-05-22 20:03:23 +0500

    refactor: Organize imports and enhance main admin panel handler functionality

--- bot/handlers/admins/__init__.py
+++ bot/handlers/admins/__init__.py
@@ -3,6 +3,8 @@ from bot.loader import FM
 
 from . import main_panel
 from . import super_admin
+from .callback_query import main_admin_panel
+
 
 # admin_settings
 if FM.feature('admin_settings'):
--- bot/handlers/admins/callback_query/main_admin_panel.py
+++ bot/handlers/admins/callback_query/main_admin_panel.py
@@ -1,51 +1,28 @@
-import logging
-from loader import dp, bot
 from aiogram import types, F
-from keyboards.inline.button import AdminCallback
-from keyboards.inline.admin_btn import main_admin_panel_btn
-from filters.admin import IsAdmin
 from aiogram.fsm.context import FSMContext
-from api.translator import translator
+
+from bot.loader import dp, bot, translator, root_logger
+from bot.keyboards.inline.button import AdminCallback
+from bot.keyboards.inline.admin_btn import main_admin_panel_btn
+from bot.filters.admin import IsAdmin
 
 
 @dp.callback_query(AdminCallback.filter(F.action == "main_adm_panel"), IsAdmin())
 async def main_panel(call: types.CallbackQuery, state: FSMContext):
-    """
-    Handles the callback query for navigating to the main admin panel.
-
-    Parameters:
-    - call (types.CallbackQuery): The callback query object containing information about the user's action.
-    - state (FSMContext): The FSM context to manage the bot's state during the conversation.
-
-    Functionality:
-    - Retrieves the admin's user ID (`user_id`), the message ID (`mid`), and the language code (`lang`) from the callback query.
-    - Translates a greeting message to the admin in their preferred language.
-    - Edits the original message in the chat to display the translated greeting and the main admin panel buttons.
-    - Updates the FSM state with the current message ID.
-
-    Returns:
-    - This function is asynchronous and does not return a value. It interacts with the Telegram API to update messages and manage the state.
-
-    Error Handling:
-    - Catches and logs any exceptions that occur during the execution, ensuring that errors are recorded for debugging.
-    """
     try:
-        user_id = call.from_user.id  # The ID of the admin who initiated the action
-        mid = call.message.message_id  # The ID of the message to be updated
-        language_code = call.from_user.language_code  # The language code for translation
+        user_id = call.from_user.id
+        mid = call.message.message_id
+        language_code = call.from_user.language_code 
 
-        # Translate the greeting message
-        text = translator(text="👩‍💻Hello, dear admin, welcome to the main panel!", dest=language_code)
+        text = translator(text="👩‍💻Hello, dear admin, welcome to the main panel!", 
+                          dest=language_code)
 
-        # Edit the message with the translated text and update it with the main admin panel buttons
         await bot.edit_message_text(chat_id=user_id,
                                     message_id=mid,
                                     text=f'{text}',
                                     reply_markup=main_admin_panel_btn(user_id=user_id, language_code=language_code))
 
-        # Update FSM state with the current message ID
         await state.update_data({"message_id": call.message.message_id})
     except Exception as err:
-        # Log any errors that occur during execution
-        logging.error(err)
+        root_logger.error(err)
 
